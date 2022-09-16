ad5ed8f18b893e2d5b9da199f594e777d2c0df2e
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2022-09-16 17:09:29 +0100

    Improve structure. Highlight available countries.

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,53 @@
-// Enable nav bar interactivity
+navInit();
+buildContent();
 
-const summaryViewButton = document.querySelector("#summary-view-button");
-const byCountryViewButton = document.querySelector("#by-country-view-button");
-const summaryView = document.querySelector(".summary-view");
-const byCountryView = document.querySelector(".by-country-view");
 
-summaryViewButton.addEventListener('click', () => {
-    summaryView.classList.remove('hidden')
-    summaryViewButton.classList.add('nav-button-active')
-    byCountryView.classList.add('hidden')
-    byCountryViewButton.classList.remove('nav-button-active')
-});
+// Enable nav interactivity
+function navInit() {
+    const summaryViewButton = document.querySelector("#summary-view-button");
+    const byCountryViewButton = document.querySelector("#by-country-view-button");
+    const summaryView = document.querySelector(".summary-view");
+    const byCountryView = document.querySelector(".by-country-view");
 
-byCountryViewButton.addEventListener('click', () => {
-    byCountryView.classList.remove('hidden')
-    byCountryViewButton.classList.add('nav-button-active')
-    summaryView.classList.add('hidden')
-    summaryViewButton.classList.remove('nav-button-active')
-});
+    summaryViewButton.addEventListener('click', () => {
+        summaryView.classList.remove('hidden')
+        summaryViewButton.classList.add('nav-button-active')
+        byCountryView.classList.add('hidden')
+        byCountryViewButton.classList.remove('nav-button-active')
+    });
+
+    byCountryViewButton.addEventListener('click', () => {
+        byCountryView.classList.remove('hidden')
+        byCountryViewButton.classList.add('nav-button-active')
+        summaryView.classList.add('hidden')
+        summaryViewButton.classList.remove('nav-button-active')
+    });
+};
 
 
 // Building content from data.json
-(async () => {
-    const getData = async () => {
-        const resp = await fetch('data/data.json');
-        return await resp.json();
-    };
+async function buildContent() {
+    const mapObject = document.querySelector('#world-map');
 
-    const data = await getData();
+    // Await for both data.json and map loaded before proceeding
+    const [data, map] = await getDataAndMap('data/data.json', mapObject);
 
+    // Build Summary View left panel
+    buildSummaryLeft(data);
+    buildByCountryMap(data, map);
+};
+
+
+// Supporting functions of buildContent()
+async function getDataAndMap(dataPath, mapObject) {
+    const [dataResp, ] = await Promise.allSettled([
+        fetch(dataPath).then(res => res.json()),
+        new Promise ((resolve) => {mapObject.addEventListener('load', resolve);})
+    ]);
+    return [dataResp.value, mapObject.contentDocument]
+};
+
+function buildSummaryLeft(data) {
     const totalSampleCount = Object.values(data['summary']['country']).reduce((a, b) => a + b);
     document.querySelector('#total-sample-count').innerHTML = Number(totalSampleCount).toLocaleString();
 
@@ -39,4 +58,15 @@ byCountryViewButton.addEventListener('click', () => {
     const totalYearValuesMin = Math.min(...totalYearValues);
     const totalYearValuesMax = Math.max(...totalYearValues);
     document.querySelector('#total-year-range-value').innerHTML = `${totalYearValuesMin} - ${totalYearValuesMax}`;
-})();
+};
+
+function buildByCountryMap(data, map) {
+    const countries = Object.keys(data['country']);
+
+    countries.forEach(country => {
+        const countryGroup = map.querySelector(`#${country}`);
+
+        // Highlight country
+        countryGroup.classList.add('country-available');
+    });
+};
